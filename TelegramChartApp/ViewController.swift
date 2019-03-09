a1090b59190003d04c621fa2e8819ea2784b865c
Igor Fedotov <igor.fedotov@brickmakers.de>
2019-03-09 18:48:39 +0100

    Initial commit

---
+++ TelegramChartApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelegramChartApp
+//
+//  Created by Igor on 09/03/2019.
+//  Copyright © 2019 Fedotov Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
