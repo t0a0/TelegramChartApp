137f86a7ff70a05c01220afc3fb09ef8ee6d70e5
Igor Fedotov <igor.fedotov@brickmakers.de>
2019-04-13 04:39:47 +0200

    remove all annotations without animation

--- TelegramChartApp/Views/Chart/ChartViews/TGCAPercentageChartView.swift
+++ TelegramChartApp/Views/Chart/ChartViews/TGCAPercentageChartView.swift
@@ -150,7 +150,10 @@ class TGCAPercentageChartView: TGCAChartView {
   
   override func removeChartAnnotation() {
     if let annotation = currentChartAnnotation as? ChartAnnotation {
+      CATransaction.begin()
+      CATransaction.setDisableActions(true)
       annotation.lineLayer.removeFromSuperlayer()
+      CATransaction.commit()
       annotation.annotationView.removeFromSuperview()
       currentChartAnnotation = nil
     }
